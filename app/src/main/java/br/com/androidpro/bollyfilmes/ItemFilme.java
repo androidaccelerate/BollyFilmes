2697eb5eb699427bab4b11c4b7813c0fb0495991
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-08-29 18:49:59 -0300

    Etapa 02

---
+++ app/src/main/java/br/com/androidpro/bollyfilmes/ItemFilme.java
@@ -0,0 +1,74 @@
+package br.com.androidpro.bollyfilmes;
+
+
+import android.net.Uri;
+
+public class ItemFilme {
+
+    private String id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private String dataLancamento;
+
+    private Uri imagem;
+
+    private float avaliacao;
+
+    public ItemFilme(String titulo, String descricao, String dataLancamento, float avaliacao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataLancamento = dataLancamento;
+        this.avaliacao = avaliacao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Uri getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(Uri imagem) {
+        this.imagem = imagem;
+    }
+
+    public float getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(float avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+}
