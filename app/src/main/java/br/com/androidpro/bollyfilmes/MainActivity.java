824c5aae733d555ba8cff5ccde69df40c28210b5
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-10-10 18:54:00 -0300

    Etapa 15

--- app/src/main/java/br/com/androidpro/bollyfilmes/MainActivity.java
+++ app/src/main/java/br/com/androidpro/bollyfilmes/MainActivity.java
@@ -7,9 +7,24 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_FILME = "FILME";
 
+    private boolean isTablet = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.fragment_filme_detalhe) != null) {
+
+            if(savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_filme_detalhe, new FilmeDetalheFragment())
+                        .commit();
+            }
+
+            isTablet = true;
+        } else {
+            isTablet = false;
+        }
     }
 }
