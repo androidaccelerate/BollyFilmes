8fea7bada959ae639b0f4f47d948d41976d5daea
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-10-31 18:54:12 -0200

    Etapa 24

---
+++ app/src/main/java/br/com/androidpro/bollyfilmes/SettingsActivity.java
@@ -0,0 +1,32 @@
+package br.com.androidpro.bollyfilmes;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String value = newValue.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(value);
+        }
+
+        return true;
+    }
+}
