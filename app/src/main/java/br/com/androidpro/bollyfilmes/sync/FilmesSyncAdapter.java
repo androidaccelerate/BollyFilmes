ce6f1f0f78fa95f00f203043df89d28c8bf07f80
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-11-29 19:34:15 -0200

    Etapa 42

---
+++ app/src/main/java/br/com/androidpro/bollyfilmes/sync/FilmesSyncAdapter.java
@@ -0,0 +1,132 @@
+package br.com.androidpro.bollyfilmes.sync;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SyncResult;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import br.com.androidpro.bollyfilmes.BuildConfig;
+import br.com.androidpro.bollyfilmes.ItemFilme;
+import br.com.androidpro.bollyfilmes.JsonUtil;
+import br.com.androidpro.bollyfilmes.R;
+import br.com.androidpro.bollyfilmes.data.FilmesContract;
+
+public class FilmesSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public FilmesSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        // https://api.themoviedb.org/3/movie/popular?api_key=qwer08776&language=pt-BR
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String ordem = preferences.getString(getContext().getString(R.string.prefs_ordem_key), "popular");
+        String idioma = preferences.getString(getContext().getString(R.string.prefs_idioma_key), "pt-BR");
+
+        try {
+            String urlBase = "https://api.themoviedb.org/3/movie/" + ordem + "?";
+            String apiKey = "api_key";
+            String language = "language";
+
+            Uri uriApi = Uri.parse(urlBase).buildUpon()
+                    .appendQueryParameter(apiKey, BuildConfig.TMDB_API_KEY)
+                    .appendQueryParameter(language, idioma)
+                    .build();
+
+            URL url = new URL(uriApi.toString());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String linha;
+            StringBuffer buffer = new StringBuffer();
+            while ((linha = reader.readLine()) != null) {
+                buffer.append(linha);
+                buffer.append("\n");
+            }
+
+            List<ItemFilme> itemFilmes = JsonUtil.fromJsonToList(buffer.toString());
+
+            if (itemFilmes == null) {
+                return;
+            }
+
+            for (ItemFilme itemFilme : itemFilmes) {
+                ContentValues values = new ContentValues();
+                values.put(FilmesContract.FilmeEntry._ID, itemFilme.getId());
+                values.put(FilmesContract.FilmeEntry.COLUMN_TITULO, itemFilme.getTitulo());
+                values.put(FilmesContract.FilmeEntry.COLUMN_DESCRICAO, itemFilme.getDescricao());
+                values.put(FilmesContract.FilmeEntry.COLUMN_POSTER_PATH, itemFilme.getPosterPath());
+                values.put(FilmesContract.FilmeEntry.COLUMN_CAPA_PATH, itemFilme.getCapaPath());
+                values.put(FilmesContract.FilmeEntry.COLUMN_AVALIACAO, itemFilme.getAvaliacao());
+                values.put(FilmesContract.FilmeEntry.COLUMN_DATA_LANCAMENTO, itemFilme.getDataLancamento());
+                values.put(FilmesContract.FilmeEntry.COLUMN_POPULARIDADE, itemFilme.getPopularidade());
+
+                int update = getContext().getContentResolver().update(FilmesContract.FilmeEntry.buildUriForFilmes(itemFilme.getId()), values, null, null);
+
+                if (update == 0) {
+                    getContext().getContentResolver().insert(FilmesContract.FilmeEntry.CONTENT_URI, values);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void syncImmediately(Context context) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        ContentResolver.requestSync(getSyncAccount(context), context.getString(R.string.content_authority), bundle);
+    }
+
+    public static Account getSyncAccount(Context context) {
+        AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+
+        Account account = new Account(context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
+
+        if(accountManager.getPassword(account) == null) {
+            if(!accountManager.addAccountExplicitly(account, "", null)) {
+                return null;
+            }
+        }
+
+        return account;
+    }
+}
--- app/src/main/java/br/com/androidpro/bollyfilmes/sync/FilmesSyncService.java
+++ app/src/main/java/br/com/androidpro/bollyfilmes/sync/FilmesSyncService.java
@@ -7,9 +7,22 @@ import android.support.annotation.Nullable;
 
 public class FilmesSyncService extends Service {
 
+    private static FilmesSyncAdapter filmesSyncAdapter = null;
+
+    private static final Object lock = new Object();
+
+    @Override
+    public void onCreate() {
+        synchronized (lock) {
+            if (filmesSyncAdapter == null) {
+                filmesSyncAdapter = new FilmesSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return filmesSyncAdapter.getSyncAdapterBinder();
     }
 }
